7cb7d477df7bbf340f2bd51de50f7ecacad619af
Artur Seletskiy <a.seletskiy@nectarin.ru>
2014-01-24 16:44:03 +0400

    changed targeting resource url

--- lib/mediasniper/api/models/targeting.rb
+++ lib/mediasniper/api/models/targeting.rb
@@ -4,6 +4,10 @@ module Mediasniper
       class Targeting < Mediasniper::Api::Abstract
         PARENT_REQUIRED = true
 
+        def default_url
+          "#{parent_url}#{entity_name}"
+        end
+
         def collection_method
           unless @response['targeting'].nil?
             self.class.new(params_for_new_instance(@response['targeting']))
